69be4396a19ae23d10e4eae0d335253905b84250
mastroiannim <michele.mastroianni@itispaleocapa.it>
2023-04-27 10:27:05 +0000

    SpringBootApplication

--- src/main/java/it/itispaleocpa/mastroiannim/HelloWorld.java
+++ src/main/java/it/itispaleocpa/mastroiannim/HelloWorld.java
@@ -1,10 +1,20 @@
 package it.itispaleocpa.mastroiannim;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
 
-
+@SpringBootApplication
+@RestController
 public class HelloWorld {
 
     public static void main(String[] args){
+        SpringApplication.run(HelloWorld.class, args);
         System.out.println("Welcome to our application");
+    }
+
+    @GetMapping("/")
+    public String getMessage() {
+        return "Hello from Docker!";
     }
 }
